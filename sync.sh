ee4dea1add18962a556322416bf3bfc18733c174
Siddharth <siddharth.vjay333@gmail.com>
2021-03-09 16:36:09 -0500

    Starter code assignment 6 + sync bash script

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+ASSIGNMENT_PATH="assignment_$1"
+ASSIGNMENT_NAMES=(
+    "1" 
+    "2" 
+    "3" 
+    "4" 
+    "5" 
+    "shaderpipeline")
+
+echo "Assignment path ${ASSIGNMENT_PATH}"
+cd $ASSIGNMENT_PATH
+cd $(ls -d */|head -n 1)
+pwd
+echo ${ASSIGNMENT_NAMES[$1-1]}
+
+git pull 
+cd build
+gcmake ..
+make
+
+
